7792036f9d7b557a78568586dcd451a650d37341
thiago <thiagosiegamg@gmail.com>
2024-10-09 11:35:42 -0300

    Aplicaçao do istalador

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from janela.Janela import Janela
+import subprocess
+
+def requisitos():
+    # Caminho do arquivo com a lista de bibliotecas e versões
+    arquivo_requisitos = "bibliotecas.txt"
+
+    # Executa o comando para instalar todas as bibliotecas e versões do arquivo
+    subprocess.run(["pip", "install", "-r", arquivo_requisitos])
+
 
 if __name__ == "__main__":
+    requisitos()
+    from janela.Janela import Janela
     app = Janela()
     app.run()
